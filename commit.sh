39e383bb522b5550a959b2bcd4d41c9c234e1a29
Lins <lindebiaoldb@gmail.com>
2019-08-01 16:07:28 +0800

    init

---
+++ commit.sh
@@ -0,0 +1,4 @@
+date >> ./README.md
+&&
+/usr/local/git/bin/git pull && /usr/local/git/bin/git status &&  /usr/local/git/bin/git add ./README.md && /usr/local/git/bin/git commit -a -m " . "'默认提交{$now}' && /usr/local/git/bin/git push
+
