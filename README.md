1b9f28410ab87b63d807139d7059072c8651246a
Lins <lindebiaoldb@gmail.com>
2019-08-01 16:32:30 +0800

    . 默认提交{$now}

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Thu Aug  1 16:13:28 CST 2019
 Thu Aug  1 16:13:46 CST 2019
 Thu Aug  1 16:14:01 CST 2019
+Thu Aug  1 16:32:29 CST 2019
+
--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 date >> ./README.md
-
+echo >> ./README.md
 /usr/local/git/bin/git pull && /usr/local/git/bin/git status &&  /usr/local/git/bin/git add ./README.md && /usr/local/git/bin/git commit -a -m " . "'默认提交{$now}' && /usr/local/git/bin/git push
 
